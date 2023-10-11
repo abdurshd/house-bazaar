668706902fd8da49f54f0b0e7d235927d2c7879e
aburashidakbarov <kakaabdurashid1995@gmail.com>
2023-10-11 20:23:16 +0900

    moved sensible info to .env files

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -9,13 +9,13 @@ import { getAnalytics } from 'firebase/analytics';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyD95TQVq-F1pOYL9Zd-cuMWc7AteLwVOP8',
-  authDomain: 'housebazaar-59524.firebaseapp.com',
-  projectId: 'housebazaar-59524',
-  storageBucket: 'housebazaar-59524.appspot.com',
-  messagingSenderId: '698484476853',
-  appId: '1:698484476853:web:66492e4a1e484dcf6367fc',
-  measurementId: 'G-7PMTMQ8ZXX',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
