905f9f75c3b3398ad0c4c97af64e49c588a7ec63
Ricky <kakaabdurashid1995@gmail.com>
2024-12-02 12:10:45 +0900

    Remove CNAME file and clean up

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.css'; // Import the CSS for styling
+
+const Spinner = () => {
+  return (
+    <div className="loadingSpinnerContainer">
+      <div className="loadingSpinner"></div>
+    </div>
+  );
+};
+
+export default Spinner; 
